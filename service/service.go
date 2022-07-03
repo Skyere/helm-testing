941fbd1f885d9a9592d15565f60030f563977c49
Skyere <taras.ishchak@gmail.com>
2022-07-03 08:52:20 +0000

    fix serviceName

--- service/service.go
+++ service/service.go
@@ -13,7 +13,7 @@ var WasServiceCheck bool = false
 
 func ServiceCheck(serviceName string, releaseName string, kubectlOptions *k8s.KubectlOptions) func(t *testing.T) {
 	return func(t *testing.T) {
-		ServiceName := fmt.Sprintf("%s-%s", serviceName, releaseName)
+		ServiceName := fmt.Sprintf("%s-%s", releaseName, serviceName)
 
 		k8s.WaitUntilServiceAvailable(t, kubectlOptions, ServiceName, 10, 5*time.Second)
 
