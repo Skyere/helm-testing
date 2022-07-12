fecb3814733acc9df4107d04bd1b4eb629992167
Skyere <taras.ishchak@gmail.com>
2022-07-12 09:08:40 +0000

    add retries adjusting

--- ingress/ingress.go
+++ ingress/ingress.go
@@ -8,10 +8,10 @@ import (
 	"github.com/gruntwork-io/terratest/modules/k8s"
 )
 
-func IngressCheck(ingressName string, releaseName string, kubectlOptions *k8s.KubectlOptions) func(t *testing.T) {
+func IngressCheck(ingressName string, releaseName string, kubectlOptions *k8s.KubectlOptions, retries int) func(t *testing.T) {
 	return func(t *testing.T) {
 		ingressName := fmt.Sprintf("%s-%s", releaseName, ingressName)
 
-		k8s.WaitUntilIngressAvailable(t, kubectlOptions, ingressName, 10, 5*time.Second)
+		k8s.WaitUntilIngressAvailable(t, kubectlOptions, ingressName, retries, 5*time.Second)
 	}
 }
--- service/service.go
+++ service/service.go
@@ -11,11 +11,11 @@ import (
 // status
 var WasServiceCheck bool = false
 
-func ServiceCheck(serviceName string, releaseName string, kubectlOptions *k8s.KubectlOptions) func(t *testing.T) {
+func ServiceCheck(serviceName string, releaseName string, kubectlOptions *k8s.KubectlOptions, retries int) func(t *testing.T) {
 	return func(t *testing.T) {
 		ServiceName := fmt.Sprintf("%s-%s", releaseName, serviceName)
 
-		k8s.WaitUntilServiceAvailable(t, kubectlOptions, ServiceName, 10, 5*time.Second)
+		k8s.WaitUntilServiceAvailable(t, kubectlOptions, ServiceName, retries, 5*time.Second)
 
 		WasServiceCheck = true
 	}
